4d76882da4d253ab13f9db2c96909b60524b47dd
Alexander Celis <alexander.celis@elenas.la>
2023-11-23 20:34:58 -0500

    Fix string formatting in app.js

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const apellido = "Perez";
 // tipos de datos primitivos
 // string -> cadena de caracteres
 const nombreEstudiante = "Frida";
-const otroNombre = 'Frida';
+const otroNombre = "Frida";
 
 // number -> números
 const edad = 28;
@@ -28,7 +28,7 @@ const precio = 12.5;
 const precioCelular = 12_000_000;
 // boolean -> valores de verdad
 const esEstudiante = true;
-const test = '100' + 100;
+const test = "100" + 100;
 // null -> vacío
 const nada = null;
 // undefined -> indefinido
@@ -42,3 +42,63 @@ let x;
 console.log(typeof x);
 
 const valor = Symbol(10);
+
+// operadores aritméticos
+// + - * / % ++ --
+let numero = 10;
+let numero2 = 20;
+let resultado = numero + numero2;
+console.log(resultado);
+
+// objetos
+const userName = "Juan";
+const userLastName = "Perez";
+const userAge = 28;
+const userIsStudent = true;
+
+// objeto -> agrupación de datos
+// clave: valor
+console.log("----Objeto----");
+const user = {
+  firstName: "Juan",
+  lastName: "Perez",
+  age: 28,
+  isStudent: true,
+  apellido: false,
+  state: {
+    name: "Bogotá",
+    country: "Colombia",
+  },
+  "color-preferido": "rojo",
+};
+
+// acceder a los valores de un objeto
+console.log(user);
+console.log(typeof user);
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.age);
+console.log(user.isStudent);
+console.log(user.state.name);
+console.log(user["color-preferido"]);
+
+console.log(user?.apellido ?? "No existe el apellido"); // null, undefined
+console.log(user?.apellido || "No existe el apellido"); // null, undefined, 0, false, ''
+console.log("false");
+console.log(-10);
+
+// arrays -> arreglos
+console.log("----Arreglos----");
+const frutas = ["manzana", "pera", "banano", "fresa", "uva"];
+const verduras = ["tomate", "cebolla", "papa", "zanahoria"];
+console.log(frutas[0]);
+console.log(frutas[2]);
+
+const frutasVerduras = [frutas, verduras];
+const frutasVerduras2 = [
+  ["manzana", "pera", "banano", "fresa", "uva"],
+  ["tomate", "cebolla", "papa", "zanahoria"],
+];
+console.log(frutas);
+console.log(frutasVerduras[1][1]); // verduras -> cebolla
+console.log(frutasVerduras2[1][1]); // verduras -> cebolla
