7d6a60202451597373503983eef40a9531920e7c
donCelis <alexcelis019@gmail.com>
2023-12-05 22:15:51 -0500

    Add name attribute to email and password fields in form

--- js/formularios.js
+++ js/formularios.js
@@ -15,17 +15,53 @@ function otraCosa() {
   console.log("otra Cosa");
 }
 
-(function (n1, n2) {
+/* (function (n1, n2) {
   console.log(n1, n2);
-})(50, 50);
+})(50, 50); */
 
-$buttonLogin.addEventListener("click", cualquierCosa);
+/* $buttonLogin.addEventListener("click", cualquierCosa);
 $buttonLogin.addEventListener("click", function () {
   console.log("cualquier cosa");
 });
 
 $buttonLogin.addEventListener("click", () => {
   console.log("cualquier cosa");
+}); */
+
+const getFormValues = (form) => Object.fromEntries(new FormData(form));
+
+$login.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // const { email, password } = event.target;
+  // ids
+  // console.log(email.value)
+  // console.log(password.value)
+
+  // name
+  // console.log(email.value);
+  // console.log(password.value)
+
+  const { email, password } = getFormValues($login);
+
+  if (email === user.email && password === user.password) {
+    console.log("ok");
+    event.target.reset();
+  } else {
+    console.log("error");
+  }
+
+  // $login.reset();
+  // event.target.reset();
+  // localStorage
 });
 
-// $login.addEventListener("submit", cualquierCosa);
+const user = {
+  name: "Romina",
+  lastName: "Arenas",
+  password: "123456",
+  email: "romina2023@correo.com",
+};
+
+/* console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.fromEntries(user)) */
